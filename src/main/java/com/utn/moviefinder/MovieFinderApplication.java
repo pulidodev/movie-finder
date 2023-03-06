d4f93ed877da34eb3f6f39593803942f99179ab9
Francisco Pulido <pulidodev@gmail.com>
2023-03-06 11:08:52 -0300

    add dependencies

---
+++ src/main/java/com/utn/moviefinder/MovieFinderApplication.java
@@ -0,0 +1,13 @@
+package com.utn.moviefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/moviefinder/MovieFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.moviefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
